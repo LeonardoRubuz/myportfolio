637795518624f6be225927631d869ebde1c06472
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-02-26 13:35:05 +0100

    main skeleton

---
+++ src/pages/home.jsx
@@ -0,0 +1,23 @@
+function Home() {
+    return ( 
+        <>
+            <div className="main-container">
+                <div className="identity">
+                    <div className="image-container">
+                        <img src="" alt="Photo de profil" />
+                    </div>
+                    <h1>Leonardo M. Rubuz</h1>
+                    <h2>Junior Software Engineer</h2>
+                </div>
+                <button>Talk to me</button>
+                <h2>Kinshasa, DRC</h2>
+                <hr />
+                <div className="socials">
+                    
+                </div>
+            </div>
+        </>
+     );
+}
+
+export default Home;
