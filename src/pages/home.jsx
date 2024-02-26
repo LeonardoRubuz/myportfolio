14aad76d84a755d67726fbfdbac5633f1af88212
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-02-26 18:04:50 +0100

    profile image set

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,10 +4,10 @@ function Home() {
             <div className="main-container">
                 <div className="identity">
                     <div className="image-container">
-                        <img src="" alt="Photo de profil" />
+                       
                     </div>
                     <h1>Leonardo M. Rubuz</h1>
-                    <h2>Junior Software Engineer</h2>
+                    <h2>Software Engineer</h2>
                 </div>
                 <button>Talk to me</button>
                 <h2>Kinshasa, DRC</h2>
